c0e236e640314cc438168f353f9a3138b16b79e7
Justin Poehnelt <jpoehnelt@google.com>
2021-11-22 14:17:54 -0700

    chore: formatting

--- rollup.config.js
+++ rollup.config.js
@@ -29,8 +29,8 @@ export default [
   {
     input: "src/index.ts",
     plugins: [
-      typescript({ tsconfig: './tsconfig.json', 'declarationDir': './' }),
-,
+      typescript({ tsconfig: "./tsconfig.json", declarationDir: "./" }),
+
       commonjs(),
       babel(babelOptions),
       terser(terserOptions),
@@ -53,8 +53,8 @@ export default [
   {
     input: "src/index.ts",
     plugins: [
-      typescript({ tsconfig: './tsconfig.json', 'declarationDir': './' }),
-,
+      typescript({ tsconfig: "./tsconfig.json", declarationDir: "./" }),
+
       commonjs(),
       babel(babelOptions),
       terser(terserOptions),
@@ -68,8 +68,9 @@ export default [
   },
   {
     input: "src/index.ts",
-    plugins: [typescript({ tsconfig: './tsconfig.json', 'declarationDir': './' }),
-],
+    plugins: [
+      typescript({ tsconfig: "./tsconfig.json", declarationDir: "./" }),
+    ],
     output: {
       file: "dist/index.esm.js",
       format: "esm",
